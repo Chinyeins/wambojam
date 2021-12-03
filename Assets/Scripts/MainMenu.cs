55e91ffcbdc6de87cbe0c6c3bad5654bbfccb957
admin <chiny1@web.de>
2021-12-03 23:20:50 +0100

    Added MainMenu Level and UI

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void startGame() {
+        Debug.Log("Starting Game");
+    }
+
+    public void exitGame() {
+        Debug.Log("Exit Game");
+    }
+}
