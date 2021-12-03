ae88b44f50afe9cc793b89d78fff62dfcf2c4af8
admin <chiny1@web.de>
2021-12-04 00:50:01 +0100

    finished game over menu

--- Assets/GameOverMenu.cs
+++ Assets/GameOverMenu.cs
@@ -5,9 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class GameOverMenu : MonoBehaviour
 {
+    public GameObject canvas;
+
+    private void Start()
+    {
+        if(this.canvas) {
+            //initally hide game over menu
+            this.canvas.SetActive(false);
+        }
+    }
+
     public void LoadMainMenu() {
         Debug.Log("Loading main menu...");
 
         SceneManager.LoadScene(0);
     }
+
+    public void showGameOver() {
+        if(canvas != null) {
+            this.canvas.SetActive(true);
+
+            this.disableUserInput();
+        }
+    }
+
+
+    protected void disableUserInput() {
+        //TODO: Disable user input
+    }
+    
 }
