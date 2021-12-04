eeaf2bb419ed20acf4a7ce5996a2d07ec8f3e05c
Dennis M. Heine <dennis.heine@gmx.de>
2021-12-04 07:48:30 +0100

    GameState inital commit
    GameState
    -Basic data structure
    -Some necessary functions
    -Commented out two parts in FirstPersonAIO.cs that prevented Unity from working
     (dork: "Dennis")
    Notes:
    The necessery snippets are currently being cached on each level load. maybe change that.

---
+++ Assets/Scripts/GameState/GameStateManager.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameStateManager : MonoBehaviour
+{
+
+    public GameState gameState;
+
+    public void collectInventoryItem(InventoryItem inventoryItem)
+    {
+        if(inventoryItem.inventoryItemType.isUnique)
+        {
+            if(gameState.inventory.inventoryItems.Contains(inventoryItem))
+                gameState.inventory.inventoryItems.Add(inventoryItem);
+        }
+        else
+        {
+            gameState.inventory.inventoryItems.Add(inventoryItem);
+        }
+    }
+
+    public void insertSnippetIntoPicture(InventoryItem inventoryItem)
+    {
+        gameState.inventory.inventoryItems.Remove(inventoryItem);
+        gameState.pictureOfMother.snippets.Add((Snippet)inventoryItem);
+    }
+
+    private bool isPictureFinished()
+    {
+        bool allFound = true;
+        foreach(SnippetType snippetTypeRequired in gameState.pictureOfMother.snippetsRequired)
+        {
+            bool snippetFound = false;
+            foreach(Snippet snippet in gameState.pictureOfMother.snippets)
+            {
+                if(snippet.snippetType == snippetTypeRequired)
+                {
+                    snippetFound = true;
+                    break;
+                }                
+            }
+            if (!snippetFound)
+            {
+                allFound = false;
+                break;
+            }
+        }
+        return allFound;
+    }
+
+    private void nextScene()
+    {
+        gameState.iteration++;
+        LevelData levelData = GameObject.Find("LevelData").GetComponent<LevelData>();
+        gameState.pictureOfMother.snippetsRequired = new List<SnippetType>();
+        gameState.pictureOfMother.snippets = new List<Snippet>();
+        
+        foreach(SnippetType snippetType in levelData.snippetTypesRequired[gameState.iteration])
+        {
+            gameState.pictureOfMother.snippetsRequired.Add(snippetType);
+        }
+
+        SceneManager.LoadScene("MainLevel");
+    
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
