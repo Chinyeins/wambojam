ead8863c42a6d2a0f167f0910138cd42e5ee69b4
admin <chiny1@web.de>
2021-12-05 06:37:59 +0100

    save

---
+++ Assets/FlickeringLight.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringLight : MonoBehaviour
+{
+    private Animation anim;
+    public float PlayAnimationEverySeconds = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animation>();
+    }
+
+    /// <summary>
+    /// Play animation - flickering, randomly every X seconds
+    /// </summary>
+    private void playAnimation() {
+        if(PlayAnimationEverySeconds > 1.0f) {
+            Invoke("playAnimation", PlayAnimationEverySeconds);
+        } 
+    }
+}
